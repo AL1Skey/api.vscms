d377aec5624b329e8d45f2225fb84423b32abebc
al1skey <adamnurramadan@gmail.com>
2025-02-09 10:00:44 +0700

    Add Laravel Sanctum for API authentication and update configuration files

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,38 +10,50 @@ class AuthController extends Controller
     //
     public function login(Request $request)
     {
-        $this->validate($request, [
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
-
-        if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
-            $token = auth()->user()->createToken('Personal Access Token')->accessToken;
-            return response()->json(['token' => $token], 200);
-        } else {
-            return response()->json(['error' => 'Unauthorized'], 401);
+        try {
+            //code...
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+    
+            if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
+                $token = auth()->user()->createToken('Personal Access Token')->plainTextToken;
+                return response()->json(['token' => $token], 200);
+            } else {
+                return response()->json(['error' => 'Unauthorized'], 401);
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json(['error' => $th->getMessage()], 401);
         }
     }
 
     public function register(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
-
-        $user = new User([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-        ]);
-
-        $user->save();
-
-        $token = $user->createToken('Personal Access Token')->accessToken;
-
-        return response()->json(['token' => $token], 201);
+        try {
+            //code...
+            $request->validate([
+                    'name' => 'required|string|max:255',
+                    'email' => 'required|string|email|max:255|unique:users',
+                    'password' => 'required|string|min:6',
+                ]);
+            // dd("user");
+                
+            $user = new User([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+            ]);
+    
+            // $token = $user->createToken('Personal Access Token')->accessToken;
+            $user->save();
+    
+            return response()->json(['msg' => "User has been succesfully created"], 201);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json(['error' => $th->getMessage()], 401);
+        }
     }
 
     public function logout(Request $request)
@@ -54,4 +66,10 @@ class AuthController extends Controller
     {
         return response()->json($request->user());
     }
+
+    public function validate($request, $array)
+    {
+        return $request->validate($array);
+    }
+    
 }
